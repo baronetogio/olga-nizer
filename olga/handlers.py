1cf866c4b6b5c7fb551724cb9dc6e2b11993a1de
baronetogio <giovcb@hotmail.com>
2022-05-07 19:07:52 -0300

    quick fixed file verification

--- olga/handlers.py
+++ olga/handlers.py
@@ -12,21 +12,21 @@ def clear_console():
 
 # USER RELATED FUNCTIONALITY
 def user_verify():
-    """ try: """
-    user_hand = open('user.json', encoding='utf-8')
-    cont = json.loads(user_hand.read())
-    user = cont["name"]
-    tasklist = cont["tasks"]
-    user_hand.close()
-    clear_console()
-    print(f'Bem vindo, {user}!\n')
-    return tasklist, user
-    """ except:
+    try:
+        user_hand = open('user.json', encoding='utf-8')
+        cont = json.loads(user_hand.read())
+        user = cont["name"]
+        tasklist = cont["tasks"]
+        user_hand.close()
+        clear_console()
+        print(f'Bem vindo, {user}!\n')
+        return tasklist, user
+    except:
         tasklist = []
         user = input('Você é novo por aqui... Qual o seu nome? ')
         clear_console()
         print(f'Bem vindo, {user}!')
-        return tasklist, user """
+        return tasklist, user
 
 def user_action(tasklist, user):
     print('O que deseja fazer?\n\n1-Adicionar uma nova tarefa\n2-Salvar e sair')
